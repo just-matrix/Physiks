754988156e90f28151c0108495ee2670e6054b1f
just-matrix <83116466+just-matrix@users.noreply.github.com>
2021-05-02 23:06:39 +0200

    Add files via upload

---
+++ epot (energie).py
@@ -0,0 +1,8 @@
+import time
+m = input("whats the mass: ")
+h = input("whats the hight: ")
+m2 = int(m)
+h2 = int(h)
+print("calculating...")
+time.sleep(2)
+print("your epot is : ", m2 * 9.81 * h2, "j" )
